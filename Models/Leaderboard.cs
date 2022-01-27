7406e0a569838fd6255c957da88ea85bd52d9c71
Michael Coffey <michaelcoffey1999@gmail.com>
2022-01-27 16:30:31 -0600

    Models

---
+++ Models/Leaderboard.cs
@@ -0,0 +1,8 @@
+namespace Models;
+
+public class Leaderboard{
+    public Leaderboard(){}
+
+    public int LeaderboardID { get; set; }
+    public int PlayerID { get; set; }
+}
---
+++ Models/Player.cs
@@ -0,0 +1,10 @@
+namespace Models;
+
+public class Player{
+    public Player(){}
+
+    public int PlayerID { get; set; }
+    public bool isMaster { get; set; }
+    public int UserID { get; set; }
+    public int Score { get; set; }
+}
