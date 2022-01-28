5b58478a7c6b517c00cb27960347db5e5f7e1fd2
Michael Coffey <michaelcoffey1999@gmail.com>
2022-01-28 13:35:57 -0600

    New controllers

---
+++ WebAPI/Controllers/UserController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using DL;
+using BL;
+using Microsoft.Extensions.Caching.Memory;
+namespace WebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+
+}
