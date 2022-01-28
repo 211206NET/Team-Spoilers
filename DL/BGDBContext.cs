04bc09631b17701d15ff0e8b0517c564ac01a9f2
Cynlaguerre <cynthia.laguerre@revature.net>
2022-01-28 17:41:55 -0500

    creating tables

--- DL/BGDBContext.cs
+++ DL/BGDBContext.cs
@@ -7,4 +7,11 @@ public class BGDBContext : DbContext
     public BGDBContext() : base() { }
     public BGDBContext(DbContextOptions options) : base(options) { }
 
+    public DbSet<BingoCard> BingoCards { get; set; }
+    public DbSet<Game> Games { get; set; }
+    public DbSet<Leaderboard> Leaderboards { get; set; }
+    public DbSet<Player> Players { get; set; }
+    public DbSet<Series> Series { get; set; }
+    public DbSet<User> Users { get; set; }
+
 }
