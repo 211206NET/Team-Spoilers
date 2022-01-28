38859847f380080aeb9c54d9f760d8c393eb7ead
Cynlaguerre <cynthia.laguerre@revature.net>
2022-01-28 17:34:46 -0500

    hooking up database

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,3 +1,7 @@
+using DL;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +11,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<BGDBContext>(options => options.UseNpgsql(
+    builder.Configuration.GetConnectionString("PostgreBGDB")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
