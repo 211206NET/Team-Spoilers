568c9452e4f1d894b44dbc073f81a601ac104805
Michael Coffey <michaelcoffey1999@gmail.com>
2022-01-30 13:07:23 -0600

    start of some player functions

--- Models/Player.cs
+++ Models/Player.cs
@@ -3,8 +3,24 @@ namespace Models;
 public class Player{
     public Player(){}
 
+    public Player(User nPlayer){
+        UserID = nPlayer.UserID;
+    }
+
     public int PlayerID { get; set; }
     public bool isMaster { get; set; }
     public int UserID { get; set; }
     public int Score { get; set; }
+
+    public void startGame(Player nGamer){
+        Game nGame = new Game();
+        nGamer.isMaster = true;
+        nGamer.Score = 0;
+    }
+
+    public void addPoints(Player contestant, BingoCard card){
+        if(card.isMarked == true){
+            contestant.Score++;
+        }
+    }
 }
