c8e5f10852bd4b673ee4469747ae93e5a1ec01d3
Michael Coffey <michaelcoffey1999@gmail.com>
2022-01-31 14:05:54 -0600

    data row functions for the leaderboard

--- Models/Leaderboard.cs
+++ Models/Leaderboard.cs
@@ -1,8 +1,23 @@
 namespace Models;
+using System.Data;
 
 public class Leaderboard{
     public Leaderboard(){}
 
+    public Leaderboard(int pID){
+        this.PlayerID = pID;
+    }
+
+    public Leaderboard(DataRow row){
+        this.LeaderboardID = (int) row["LeaderboardID"];
+        this.PlayerID = (int) row["PlayerID"];
+    }
+
     public int LeaderboardID { get; set; }
     public int PlayerID { get; set; }
+
+    public void ToDataRow(ref DataRow row){
+        row["LeaderboardID"] = (int) this.LeaderboardID;
+        row["PlayerID"] = (int) this.PlayerID;
+    }
 }
