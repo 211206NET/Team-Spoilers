68edb48e92247a7e01cc09fb6f2df1a389c431fa
Michael Coffey <michaelcoffey1999@gmail.com>
2022-02-03 09:56:41 -0600

    minor fix

--- WebAPI/Controllers/LeaderboardController.cs
+++ WebAPI/Controllers/LeaderboardController.cs
@@ -15,11 +15,11 @@ public class LeaderboardController : ControllerBase
         _bl = bl;
     }
     // GET: api/values
-    [HttpGet]
-    public IEnumerable<string> Get()
-    {
-        return new string[] { "value1", "value2" };
-    }
+    // [HttpGet]
+    // public IEnumerable<string> Get()
+    // {
+    //     return new string[] { "value1", "value2" };
+    // }
 
     // GET: api/values/5
     [HttpGet("{id}")]
